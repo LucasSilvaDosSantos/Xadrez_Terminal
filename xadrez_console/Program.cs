f11b86f58f96d7b02b41bae6e9ccf5bcfd7a3999
LucasSilvaDosSantos <santos94us@hotmail.com>
2019-02-15 17:51:25 -0200

    Posicao final pronta

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -7,20 +7,30 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+            System.Console.WriteLine(pos);
+
+            System.Console.WriteLine(pos.ToPosicao());
+
+            /*try
             {
+
+
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 7));
                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 2));
-
+                
                 Tela.Imprimirtabuleiro(tab);
             }
             catch(TabuleiroException e)
             {
                 System.Console.WriteLine(e.Message);
-            }
+            }*/
+
+
         }
     }
 }
